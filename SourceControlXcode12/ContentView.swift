9017eadd89e022cba91c16ea748564ab5ed4ad51
Aditya Mahesh <amahesh20@outlook.com>
2021-06-02 17:08:29 +0100

    added Xcode 12 test

---
+++ SourceControlXcode12/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SourceControlXcode12
+//
+//  Created by Aditya Mahesh on 02/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! Xcode 12 Branch")
+            .padding()
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Aditya Mahesh on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
